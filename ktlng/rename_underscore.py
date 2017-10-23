bce385989e53220e992e085c45b72f9690c31e2c
l1nk <l1nk@jpcp.net>
2017-10-22 19:33:05 -0700

    master [updating resume, updating link to insta]

---
+++ ktlng/rename_underscore.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+print "Hello. I am KTLNG.\n"
+print "You have accessed one of my file renaming modules.\n"
+print "What is the path of the directory whose filenames you'd like to change?\n"
