1f11e2dd2526b83e1a8d90476cb397aa24cc36f7
l1nk <l1nk@jpcp.net>
2020-05-08 17:50:56 -0700

    master [removing helper scripts from webpage]

--- ktlng/rename_underscore.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-print "Hello. I am KTLNG.\n"
-print "You have accessed one of my file renaming modules.\n"
-print "What is the path of the directory whose filenames you'd like to change?\n"
